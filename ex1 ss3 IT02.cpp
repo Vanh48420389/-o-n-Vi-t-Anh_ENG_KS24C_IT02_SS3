33e9f48793918af0461a052961ee7c081d690129
Vanh48420389 <sivanh740@gmail.com>
2024-11-21 16:55:18 +0700

    Add files via upload

---
+++ ex1 ss3 IT02.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	//nhap va xuat thong tin ten  
+    char Name[50]; 
+	printf("nhap ten cua ban : ");
+	scanf("%s", &Name);
+	//in ra  
+	printf("xin chao %s", Name);
+	return 0; 
+} 
---
+++ ex2 ss3 IT02.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	//khai bao thong  tin do C 
+	int C;
+	printf("nhap do : ");
+	scanf("%d", &C);
+	int F = C*9/5 +32; 
+	printf("F = %d ", F); 
+	return 0; 
+} 
